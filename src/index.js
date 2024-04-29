6816ecae0ba0e9f33c21736b79a8c69bb4f1308b
Safni-hsn <2020e136@eng.jfn.ac.lk>
2024-04-29 11:06:59 +0530

    maHomePage

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './components/home';
+import Dashboard from './Pages/maHomePage';
 
-import Auth from './components/auth';
-import Database from './components/databse';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <Home/>
-   <Auth/>
-   <Database/>
-     </React.StrictMode>
+    <Dashboard />
+  </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
